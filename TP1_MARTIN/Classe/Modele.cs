063cd4a9c9f6e69e0f30929645b5577b7d8e8538
emartin <emartin@E201-10>
2025-09-09 10:50:36 +0200

    Ajoutez des fichiers projet.

---
+++ TP1_MARTIN/Classe/Modele.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP1_MARTIN.Entities;
+
+namespace TP1_MARTIN.Classe
+{
+    public static class Modele
+    {
+        private static BdPartitionsContext monModel;
+        public static void init()
+        {
+            monModel = new BdPartitionsContext();
+        }
+        public static List<Client> listeClients()
+        {
+            return monModel.Clients.ToList();
+        }
+    }
+    
+}
